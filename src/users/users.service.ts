bffff73227baf517fde1cb15936baab000a62cf7
Erick Jean <erickjean36@gmail.com>
2026-05-24 10:50:02 -0400

    feat: remove unused user DTOs and controller; add response DTO for user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ResponseUserDto } from './dto/response-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findUserByEmail(email: string) {
-    return this.prisma.user.findUnique({
+  async findUserByEmail(email: string): Promise<ResponseUserDto> {
+    const user = await this.prisma.user.findUnique({
       where: { email },
     });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
   }
 }
